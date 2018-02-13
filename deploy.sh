5c3bc91d6937837e4a2f2a1596a668b54ebf93c9
Kye Maloy <kyemaloy2013@gmail.com>
2018-02-13 11:31:18 +0000

    Added `bx` to `wsk` commands

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ function usage() {
 
 function install() {
   echo "Creating publisher package"
-  wsk package create publisher
+  bx wsk package create publisher
 
   echo "Creating actions"
-  wsk action create -t 300000 publisher/jekyll --docker $JEKYLL_DOCKER_IMAGE\
+  bx wsk action create -t 300000 publisher/jekyll --docker $JEKYLL_DOCKER_IMAGE\
     -p OS_USER_ID "$OS_USER_ID" \
     -p OS_PASSWORD "$OS_PASSWORD" \
     -p OS_PROJECT_ID "$OS_PROJECT_ID" \
@@ -18,52 +18,52 @@ function install() {
     -p OS_CONTAINER_NAME "$OS_CONTAINER_NAME"
 
   echo "Binding GitHub"
-  wsk package bind /whisk.system/github publisher-github --param username $GITHUB_USERNAME --param repository $GITHUB_REPO --param accessToken $GITHUB_TOKEN
+  bx wsk package bind /whisk.system/github publisher-github --param username $GITHUB_USERNAME --param repository $GITHUB_REPO --param accessToken $GITHUB_TOKEN
 
   echo "Listening to GitHub push events"
-  wsk trigger create publisher-github-trigger --feed publisher-github/webhook --param events push
+  bx wsk trigger create publisher-github-trigger --feed publisher-github/webhook --param events push
 
   echo "Creating Github event change listener"
-  wsk action create publisher-github-changelistener changelistener.js
+  bx wsk action create publisher-github-changelistener changelistener.js
 
   echo "Enabling change listener"
-  wsk rule create publisher-github-rule publisher-github-trigger publisher-github-changelistener
+  bx wsk rule create publisher-github-rule publisher-github-trigger publisher-github-changelistener
 
-  wsk list
+  bx wsk list
 }
 
 function uninstall() {
   echo "Removing actions..."
-  wsk action delete publisher/jekyll
+  bx wsk action delete publisher/jekyll
 
   echo "Removing rule..."
-  wsk rule disable publisher-github-rule
-  wsk rule delete publisher-github-rule
+  bx wsk rule disable publisher-github-rule
+  bx wsk rule delete publisher-github-rule
 
   echo "Removing change listener..."
-  wsk action delete publisher-github-changelistener
+  bx wsk action delete publisher-github-changelistener
 
   echo "Removing trigger..."
-  wsk trigger delete publisher-github-trigger
+  bx wsk trigger delete publisher-github-trigger
 
   echo "Removing packages..."
-  wsk package delete publisher-github
-  wsk package delete publisher
+  bx wsk package delete publisher-github
+  bx wsk package delete publisher
 
   echo "Done"
-  wsk list
+  bx wsk list
 }
 
 function update() {
-  wsk action update publisher-github-changelistener changelistener.js
+  bx wsk action update publisher-github-changelistener changelistener.js
 }
 
 function disable() {
-  wsk rule disable publisher-github-rule
+  bx wsk rule disable publisher-github-rule
 }
 
 function enable() {
-  wsk rule enable publisher-github-rule
+  bx wsk rule enable publisher-github-rule
 }
 
 function showenv() {
